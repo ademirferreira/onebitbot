f2f7d59304e8f8e2e0947a360f4d2295a5379c09
Ademir Ferreira <ademir.ae86@gmail.com>
2020-07-05 13:46:29 -0300

    Criação do webhook

--- app.rb
+++ app.rb
@@ -1,13 +1,44 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
 
 require './config/database'
 
-Dir["./app/models/*.rb"].each {|file| require file }
+Dir['./app/models/*.rb'].sort.each { |file| require file }
+Dir['./app/services/**/*.rb'].sort.each { |file| require file }
 
 class App < Sinatra::Base
   get '/' do
     'Hello World'
+  end
+
+  post '/webhook' do
+    request.body.rewind
+    result = JSON.parse(request.body.read)['queryResult']
+
+    if result['contexts'].present?
+      response = InterpretService.call(result['action'], result['contexts'][0]['parameters'])
+    else
+      response = InterpretService.call(result['action'], result['parameters'])
+    end
+
+    content_type :json, charset: 'utf-8'
+    {
+      "payload": {
+        "telegram": {
+          "text": response,
+          "parse_mode": 'Markdown'
+        }
+      }
+    }.to_json
+
+    # content_type :json
+    # {
+    #   "speech": response,
+    #   "displayText": response,
+    #   "source": 'OneBitBot'
+    # }.to_json
   end
 end
